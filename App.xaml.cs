094b634d06097bcbca55a675580c644858ed0c94
ItumelengRandyMalau <itumelengrandy5@gmail.com>
2025-02-06 11:14:13 +0200

    assignment seven complete

--- App.xaml.cs
+++ App.xaml.cs
@@ -7,6 +7,7 @@
             InitializeComponent();
 
             MainPage = new GlobalOneHomePage();
+            MainPage = new NavigationPage(new DashboardPage());
         }
     }
 }
--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -6,4 +6,19 @@ public partial class LoginPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private  async void Button_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new GlobalOneHomePage());
+    }
+
+    private async void Button_Clicked_1(object sender, EventArgs e)
+    {
+        await Navigation.PopAsync();
+    }
+
+    private  async void Button_Clicked_2(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new GlobalOneHomePage());
+    }
 }
